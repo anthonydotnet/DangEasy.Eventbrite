a8bc0e627cbd17d5df6d77c13b46b07bc300c6d0
Anthony Dang <anthonydotnet@gmail.com>
2021-08-05 09:22:35 +0100

    Made dates more obvious for event scaffolding

--- tests/Library.Tests/BaseEventSetup.cs
+++ tests/Library.Tests/BaseEventSetup.cs
@@ -22,10 +22,11 @@ namespace Library.Tests
         // event test data
         public DateTime Data_ExecutionStartUtc;
         public DateTime Data_StartLocal;
+        public DateTime Data_EndLocal;
 
         public DateTime Data_StartUtc;
         public DateTime Data_EndUtc;
-        public string Data_Timezone = "Australia/Sydney";
+        public string Data_Timezone = "Europe/London";
         public string Data_Title = "My Event";
         public string Data_Currency = "USD";
         public bool Data_OnlineEvent = true;
@@ -40,7 +41,10 @@ namespace Library.Tests
             Service = new EventbriteService(ApiUrl, Token);
 
             Data_ExecutionStartUtc = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour, DateTime.UtcNow.Minute, 0, 0);
-            Data_StartLocal = new DateTime(2021, 1, 12, 17, 0, 0, 0);
+
+            var tomorrow = DateTime.UtcNow.AddDays(1); // create an event for tomorrow
+            Data_StartLocal = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 17, 0, 0, 0);
+            Data_EndLocal = Data_StartLocal.AddHours(1);
             ScaffoldEvent();
         }
 
@@ -53,11 +57,11 @@ namespace Library.Tests
         }
 
 
-
         public Event ScaffoldEvent()
         {
             Data_StartUtc = Data_StartLocal.ToUniversalTime(); //Data_ExecutionStartUtc.AddDays(1);
             Data_EndUtc = Data_StartUtc.AddHours(1);
+
 
             var @event = RequestModelBuilder.BuildEvent(Data_Title, Data_StartUtc, Data_EndUtc, Data_Timezone, Data_Currency);          
 
@@ -67,8 +71,8 @@ namespace Library.Tests
                 Event = new Event()
                 {
                     OrganizationId = 1234567890,
-                    Start = new EventDate { Utc = Data_StartUtc, Timezone = Data_Timezone },
-                    End = new EventDate { Utc = Data_EndUtc, Timezone = Data_Timezone },
+                    Start = new EventDate { Local = Data_StartLocal, Utc = Data_StartUtc, Timezone = Data_Timezone },
+                    End = new EventDate { Local = Data_EndLocal, Utc = Data_EndUtc, Timezone = Data_Timezone },
                 };
             }
             else
